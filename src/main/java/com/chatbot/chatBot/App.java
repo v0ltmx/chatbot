54ef8b46a60f8012171083924655098a6926b82d
Lucas Gabriel <37782940+v0ltmx@users.noreply.github.com>
2021-08-20 21:25:10 -0300

    App att

--- src/main/java/com/chatbot/chatBot/App.java
+++ src/main/java/com/chatbot/chatBot/App.java
@@ -1,13 +1,8 @@
 package com.chatbot.chatBot;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hi there, i'm VmxBot!" );
     }
 }
